6b0004775f1534c4bf98365175d8672983a3338f
cherryday <tcarevmaxim@gmail.com>
2022-05-28 16:03:49 +0300

    add BaseButton.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BaseButton from './components/BaseButton.vue';
 import GameBoard from './components/GameBoard.vue';
 import { useGame } from './composables/useGame';
 
@@ -8,7 +9,7 @@ const { tiles, restart, mergeTiles } = useGame();
 <template>
   <main class="main">
     <div class="container">
-      <button class="button" @click="restart">New Game</button>
+      <BaseButton @click="restart">New Game</BaseButton>
 
       <GameBoard :tiles="tiles" @merge="mergeTiles" />
     </div>
@@ -22,18 +23,5 @@ const { tiles, restart, mergeTiles } = useGame();
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-.button {
-  background: #8f7a66;
-  color: #f9f6f2;
-  cursor: pointer;
-  height: 40px;
-  padding: 0 20px;
-  border: none;
-  border-radius: 3px;
-  margin-bottom: 40px;
-  font-weight: bold;
-  font-size: 18px;
 }
 </style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="base-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.base-button {
+  background: #8f7a66;
+  color: #f9f6f2;
+  cursor: pointer;
+  height: 40px;
+  padding: 0 20px;
+  border: none;
+  border-radius: 3px;
+  margin-bottom: 40px;
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
