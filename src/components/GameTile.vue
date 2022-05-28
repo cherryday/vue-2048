c7f70f4da38fed0135b778c185f2f7f7b478e05e
cherryday <tcarevmaxim@gmail.com>
2022-05-28 14:07:18 +0300

    add classes

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -41,6 +41,12 @@ const classesInner = computed(() => {
     'game-tile--8': props.tile.value === 8,
     'game-tile--16': props.tile.value === 16,
     'game-tile--32': props.tile.value === 32,
+    'game-tile--64': props.tile.value === 64,
+    'game-tile--128': props.tile.value === 128,
+    'game-tile--256': props.tile.value === 256,
+    'game-tile--512': props.tile.value === 512,
+    'game-tile--1024': props.tile.value === 1024,
+    'game-tile--2048': props.tile.value === 2048,
   };
 });
 
@@ -76,7 +82,7 @@ function onAnimationEnd() {
 .game-tile {
   position: absolute;
   z-index: 4;
-  transition: transform 140ms ease-in-out;
+  transition: transform 100ms ease-in-out;
 }
 
 .game-tile__inner {
@@ -87,7 +93,7 @@ function onAnimationEnd() {
   justify-content: center;
   color: #776e65;
   background-color: #eee4da;
-  font-size: 54px;
+  font-size: 55px;
   font-weight: bold;
   font-family: sans-serif;
   border-radius: 4px;
@@ -99,11 +105,11 @@ function onAnimationEnd() {
 }
 
 .game-tile--scale {
-  animation: scale 100ms ease-in-out;
+  animation: scale 200ms ease;
 }
 
 .game-tile--new {
-  animation: new 300ms ease-in-out;
+  animation: new 200ms ease;
 }
 
 .game-tile--2 {
@@ -122,12 +128,57 @@ function onAnimationEnd() {
   color: #f9f6f2;
   background-color: #f69664;
 }
+.game-tile--32 {
+  color: #f9f6f2;
+  background-color: #f77c5f;
+}
+.game-tile--64 {
+  color: #f9f6f2;
+  background-color: #f75f3b;
+}
+.game-tile--128 {
+  color: #f9f6f2;
+  background-color: #edd073;
+  box-shadow: 0 0 30px 10px rgb(243 215 116 / 24%),
+    inset 0 0 0 1px rgb(255 255 255 / 14%);
+  font-size: 45px;
+}
+.game-tile--256 {
+  color: #f9f6f2;
+  background: #edcc62;
+  box-shadow: 0 0 30px 10px rgb(243 215 116 / 32%),
+    inset 0 0 0 1px rgb(255 255 255 / 19%);
+  font-size: 45px;
+}
+.game-tile--512 {
+  color: #f9f6f2;
+  background: #edc950;
+  box-shadow: 0 0 30px 10px rgb(243 215 116 / 40%),
+    inset 0 0 0 1px rgb(255 255 255 / 24%);
+  font-size: 45px;
+}
+.game-tile--1024 {
+  color: #f9f6f2;
+  background: #edc53f;
+  box-shadow: 0 0 30px 10px rgb(243 215 116 / 48%),
+    inset 0 0 0 1px rgb(255 255 255 / 29%);
+  font-size: 35px;
+}
+.game-tile--2048 {
+  color: #f9f6f2;
+  background: #edc22e;
+  box-shadow: 0 0 30px 10px rgb(243 215 116 / 56%),
+    inset 0 0 0 1px rgb(255 255 255 / 33%);
+  font-size: 35px;
+}
 
 @keyframes new {
   from {
+    opacity: 0;
     transform: scale(0);
   }
   to {
+    opacity: 1;
     transform: scale(1);
   }
 }
@@ -137,7 +188,7 @@ function onAnimationEnd() {
     transform: scale(1);
   }
   50% {
-    transform: scale(1.1);
+    transform: scale(1.2);
   }
   100% {
     transform: scale(1);
