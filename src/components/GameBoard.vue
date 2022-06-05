3f55bf1d63be0580585236844d5c93da314197ff
cherryday <tcarevmaxim@gmail.com>
2022-06-05 20:04:59 +0300

    Game message

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,5 +1,7 @@
 <script setup>
 import GameTile from './GameTile.vue';
+import GameMessage from './GameMessage.vue';
+import BaseButton from './BaseButton.vue';
 import { TILE_SIZE, TILE_PADDING, BOARD_SIZE } from '../constants';
 
 const emit = defineEmits(['merge']);
@@ -7,6 +9,14 @@ const emit = defineEmits(['merge']);
 const props = defineProps({
   tiles: {
     type: Array,
+    required: true,
+  },
+  win: {
+    type: Boolean,
+    required: true,
+  },
+  loss: {
+    type: Boolean,
     required: true,
   },
 });
@@ -20,6 +30,23 @@ const styleVars = {
 
 <template>
   <div class="game-board" :style="styleVars">
+    <Transition name="fade">
+      <GameMessage v-if="win" class="game-win">
+        You win!
+        <template #action>
+          <BaseButton>Keep going</BaseButton>
+          <BaseButton>Try again</BaseButton>
+        </template>
+      </GameMessage>
+
+      <GameMessage v-else-if="loss" class="game-over">
+        Game over!
+        <template #action>
+          <BaseButton>Try again</BaseButton>
+        </template>
+      </GameMessage>
+    </Transition>
+
     <div
       v-for="index in BOARD_SIZE * BOARD_SIZE"
       :key="index"
@@ -45,10 +72,37 @@ const styleVars = {
   background-color: #bbac9f;
   border-radius: 4px;
   padding: 15px;
+  overflow: hidden;
 }
 
 .game-board__tile {
   background-color: #ccc0b3;
   border-radius: 4px;
+}
+
+.game-win {
+  background-color: rgba(237, 194, 46, 0.5);
+}
+
+.game-win .game-message__title {
+  color: #f9f6f2;
+}
+
+.game-over {
+  background-color: rgba(238, 228, 218, 0.73);
+}
+
+.game-over .game-message__title {
+  color: #776e65;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/GameMessage.vue
+++ src/components/GameMessage.vue
@@ -1,33 +1,36 @@
-<script setup>
-import BaseButton from './BaseButton.vue';
-</script>
-
 <template>
-  <div class="game-over">
-    <p class="game-over__title">Game over!</p>
-    <BaseButton>Try again</BaseButton>
+  <div class="game-message">
+    <div class="game-message__title">
+      <slot></slot>
+    </div>
+    <div class="game-message__actions">
+      <slot name="action"></slot>
+    </div>
   </div>
 </template>
 
 <style>
-.game-over {
+.game-message {
   z-index: 8;
   position: absolute;
   top: 0;
   left: 0;
   height: 100%;
   width: 100%;
-  background-color: rgba(238, 228, 218, 0.73);
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
 }
 
-.game-over__title {
+.game-message__title {
   font-size: 60px;
   font-weight: bold;
-  color: #776e65;
   margin-bottom: 40px;
+}
+
+.game-message__actions {
+  display: flex;
+  gap: 10px;
 }
 </style>
